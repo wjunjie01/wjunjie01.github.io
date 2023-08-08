ca002ac05f74aa96e86dcf690cba4cc42a4e1926
wjunjie01 <junjiework48@gmail.com>
2023-08-08 20:55:14 +0800

    Added js and id

---
+++ My_webpage/js/main.js
@@ -0,0 +1,21 @@
+/*Create scrolling effect, to the currently visible section on the page*/
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 70;
+        let sectionId = current.getAttribute('id')
+
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav_items a[href*=' + sectionId + ']').classList.add('active-link')
+        }
+        else{
+            document.querySelector('.nav_items a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+}
+
+window.addEventListener('scroll', scrollActive)
